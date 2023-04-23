3721baf14cb649c7ed77b07a1fa76e6faab181bd
Diego11Arista <126733542+Diego11Arista@users.noreply.github.com>
2023-04-22 19:35:45 -0600

    Add files via upload

---
+++ Ahorcado.py
@@ -0,0 +1,14 @@
+#Ahorcado proyecto.
+#Importar las herramientas que sirven para.
+import
+#Importar las herramientas que sirven para.
+import
+#Importar las herramientas que sirven para.
+import
+print('Bienvenido al juego del ahorcado ')
+
+#v son las vidas, o intentos que tendra el jugador para completar la palabra.
+v=7
+while start=='si':
+#p sera la variable para guardar una letra que sustituira un epacio de la palabra si son iguales.
+p=input('Cual es la letra que eliges?')
