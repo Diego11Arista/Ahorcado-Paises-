49cfcb8747e385cffed86d424add2e00589189d3
Diego11Arista <126733542+Diego11Arista@users.noreply.github.com>
2023-04-26 17:50:44 -0600

    Update Ahorcado.py

--- Ahorcado.py
+++ Ahorcado.py
@@ -1,14 +1,20 @@
 #Ahorcado proyecto.
-#Importar las herramientas que sirven para.
-import
-#Importar las herramientas que sirven para.
-import
-#Importar las herramientas que sirven para.
-import
+#Importar las herramientas que sirven para cambiar de color los textos impresos.
+import colors
+#Importar las herramientas que sirven para crear tiempos de espera.
+import time
+#Importar las herramientas que sirven para principal (Limpiar pantalla).
+import os
 print('Bienvenido al juego del ahorcado ')
-
+#Empieza el ciclo while que contendra las 10 palabras.
+start=input('Quieres empezar el juego?')
 #v son las vidas, o intentos que tendra el jugador para completar la palabra.
 v=7
-while start=='si':
+if start=='si':
+    while start=='si':
+        print('La primera preguanta es /n ')
+else:
+    print('Entiendo que no estes list@ para empezar es un desafio dificil :)')
+    start=input('Ya estas list@ para empezar?')
 #p sera la variable para guardar una letra que sustituira un epacio de la palabra si son iguales.
 p=input('Cual es la letra que eliges?')
